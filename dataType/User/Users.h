bed26ff284f6bc7978c5075e9a3656f3e82050d3
Ragnar-exe <53235270+Lionjj@users.noreply.github.com>
2021-05-03 21:35:22 +0200

    struttura utente
    definizione della struttura utente. P.S. sto inserendo ciascuno dei file header in una sotto directory di "dataType" così che poi possiamo anche inserire un file con estensione .c dove inseriamo tutte le funzioni/procedure.

---
+++ dataType/User/Users.h
@@ -0,0 +1,13 @@
+#ifndef GALLERIADARTE_USERS_H
+#define GALLERIADARTE_USERS_H
+
+typedef struct
+{
+    char name[30];
+    char surname[30];
+    char username[50];
+    char email[80];
+    char pw [50];
+}Users;
+
+#endif //GALLERIADARTE_USERS_H
