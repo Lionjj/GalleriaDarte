fc2adb660f8c19a6135e81ba70a94dc25f97b4a5
Ragnar-exe <53235270+Lionjj@users.noreply.github.com>
2021-05-03 21:24:49 +0200

    Eliminazione del file artshow.c
    ho eliminato questo file perché in sostanza non riuscivo a dichiarare nessuna delle strutture nel main, perciò per ora ho inserito le strutture direttamente nel header file.
    Quindi così facendo non c'è bisogno di dichiarare un puntatore a alla struttura.

--- dataTipe/ArtShow/artshow.h
+++ dataTipe/ArtShow/artshow.h
@@ -1,12 +1,51 @@
-//
-// Created by iMuSL on 03/05/2021.
-//
-
 #ifndef GALLERIADARTE_ARTSHOW_H
 #define GALLERIADARTE_ARTSHOW_H
 
-typedef struct exhiPlace exhiPlace;
+#include <stdbool.h>
 
-typedef struct exposure exposure;
+typedef struct
+{
+    char streetName[90];
+    char houseNum[5];
+    char city[30];
+    char structure[50];
+    unsigned int paintingsNumb;
+    unsigned int sculpturesNumb;
+}exhiPlace;
+
+typedef struct{
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
+}exposure;
+
+typedef struct
+{
+    char name[30];
+    char surname[30];
+    char fiscalCode[16];
+    char IDResponsible[6];
+}loacaManager;
+
+
+enum operaType {painting, sculpture, drawing };
+
+typedef struct
+{
+    char name[100];
+    char authorName[30];
+    char authorSurname[30];
+    enum operaType operaType;
+    char kind [30];
+    char historPeriod[30];
+    unsigned int prodYear;
+    bool BC;
+}artwork;
+
+bool tempExhibition;
+
+unsigned int sizeShows;
+
+unsigned int codeShows;
 
 #endif //GALLERIADARTE_ARTSHOW_H
